f3a6e92259d306c54479465b9229e2283d3dbacf
Jedith Almando Istian <93966961+jevsuniverse@users.noreply.github.com>
2025-10-15 13:41:54 +0700

    push ner

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ $('a.nav-link').click(function (e) {
   scrollTo(href);
 });
 
-const textArray = ["Data Engineer", "Data Analyst", "Front end", "UI/UX"];
+const textArray = ["Data Engineer", "Data Analyst", "Front end", "UI/UX", "Back end"];
 const typingDelay = 90; // Delay in milliseconds between typing characters
 const newTextDelay = 400; // Delay in milliseconds before typing the next text
 let textIndex = 0;
